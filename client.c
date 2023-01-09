82851fc9f00541b42fab1a644e0085a68fd87b89
UNIX IO <bertinfonge0@gmail.com>
2023-01-09 22:03:42 +0100

    commit

--- client.c
+++ client.c
@@ -1,135 +1,143 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
 #include <sys/types.h>
-char buffer[1024];
-int option; //store the values of the menu
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
-int execute(char buffer[1024]){
-	while(1){
-		if(system(buffer) != 0){       //ie what is in buffer is ran by the sys cmd in variable called buffer
-			break;                   //if it executes and returnes value diff from zero implies failure hence break
-		}
-		fgets(buffer, 1024, stdin);     //collects from second input etc and executes here
-		
-	
-   }
-return 0;
+#define LENGTH 2048
+
+// Global variables
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
+char name[32];
+
+void str_overwrite_stdout() {
+  printf("%s", "> ");
+  fflush(stdout);
 }
 
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { // trim \n
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
+}
 
+void catch_ctrl_c_and_exit(int sig) {
+    flag = 1;
+}
+
+void send_msg_handler() {
+  char message[LENGTH] = {};
+	char buffer[LENGTH + 32] = {};
+
+  while(1) {
+  	str_overwrite_stdout();
+    fgets(message, LENGTH, stdin);
+    str_trim_lf(message, LENGTH);
+
+    if (strcmp(message, "bye") == 0) {
+			break;
+    } else {
+      sprintf(buffer, "%s: %s\n", name, message);
+      send(sockfd, buffer, strlen(buffer), 0);
+    }
+
+		bzero(message, LENGTH);
+    bzero(buffer, LENGTH + 32);
+  }
+  catch_ctrl_c_and_exit(2);
+}
+
+void recv_msg_handler() {
+	char message[LENGTH] = {};
+  while (1) {
+		int receive = recv(sockfd, message, LENGTH, 0);
+    if (receive > 0) {
+      printf("%s", message);
+      str_overwrite_stdout();
+    } else if (receive == 0) {
+			break;
+    } else {
+			// -1
+		}
+		memset(message, 0, sizeof(message));
+  }
+}
 
 int main(int argc, char * argv[]){
- 		  char *ip;
-		  int port;		//stores value of port number
-
-		  int sock;
-		  struct sockaddr_in addr;
-		  socklen_t addr_size;
-		  char buffer[1024];
-		  int n;
-		  
-		  if (argc<3)		//stores filename? ip and port num indexes ie lenght of array
-		  {
-		    printf("provide required command-line inputs as- <filename.c> <server_IP_addr> <port>");
-		    exit(1);
-		  }
-		  port = atoi(argv[2]);		 //atoi is predefined func ie at position 2 get port num
-		  sock = socket(AF_INET, SOCK_STREAM, 0);             //default
-		  // check if socket is created properly
-		  if (sock < 0){			//ie if one of the parameters above is missing, error
-		    perror("[-]Socket error");
-		    exit(1);
-		  }
-		  printf("[+]TCP server socket created.\n");
-
-		  //memset(&addr, '\0', sizeof(addr));
-		  addr.sin_family = AF_INET;
-		  addr.sin_port = htons(port);
-		  addr.sin_addr.s_addr = inet_addr(argv[1]);
-
-		  if(connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0)
-		  {
-		    printf("Connection failed...!!!");
-		    exit(1);
-		  }
-		  printf("Connected to the server.\n");
-
-printf("################################################################");
-printf("\nWelcome Client to NetApp");
-printf("\nWHich feature do you want to use, select by number");
-
-while(1){		 //ensures you don't leave the application
-	printf("\n1: Messaging\n2: Running Commands\n3: File Sharing\n4: Exit\n");
-	scanf("%d", &option);
-	if(option == 1){
-		 
-		  while(1)
-		  {
-
-		  bzero(buffer, 1024); // clear buffer
-		  fgets(buffer, 1024, stdin);
-		  //execute(buffer);
-		  //strcpy(buffer, msg);
-		 //printf("Client --> %s\n", buffer);
-		  n = write(sock, buffer, strlen(buffer));		//here client writes message to server
-		  if (n <0)
-		  {
-		  	printf("Error in writing...");
-		  	exit(1);
-		  }
-
-		  bzero(buffer, 1024); // clear buffer
-		  n = read(sock, buffer, 1024);			//here client reads message from server
-		  if (n<0)
-		  {
-		  	printf("Error in reading...");
-		  	exit(1);
-		  }
-		  printf("\nServer --> %s", buffer);
-		  
-		  if (!strncmp("end", buffer, 3))
-		  	break;
-		}
-
-		  close(sock);
-		  printf("Disconnected from the server.\n");
-
-				   
-			
-	}
-	else if(option == 2){
-		printf("Enter a command\n");
-		fgets(buffer, 1024, stdin);		//very effective compared to scanf
-		execute(buffer);
-	
-	}
-	else if(option == 3){
-		break;
-	//enter code for file transfer
-	}
-	else if(option == 4){
-		break;
-	}
-	else{
-		printf("\n %d is an invalid input\n", option);
+	if(argc < 2){
+		//printf("Usage: %s <port>\n", argv[0]);
+		printf("provide required command line inputs as <filename.c> <server_IP_addr> <portno>");
+		//return EXIT_FAILURE;
+		exit(1);
 	}
 
+	//char *ip = "127.0.0.1";
+	int port = atoi(argv[2]);
+
+	signal(SIGINT, catch_ctrl_c_and_exit);
+
+	printf("Please enter your name: ");
+  fgets(name, 32, stdin);
+  str_trim_lf(name, strlen(name));
 
 
-}
+	if (strlen(name) > 32 || strlen(name) < 2){
+		printf("Name must be less than 30 and more than 2 characters.\n");
+		return EXIT_FAILURE;
+	}
+
+	struct sockaddr_in server_addr;
+
+	/* Socket settings */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+  server_addr.sin_port = htons(port);
 
 
+  // Connect to Server
+  int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (err < 0) {
+		printf("ERROR: connect\n");
+		//return EXIT_FAILURE;
+		exit(1);
+	}
 
+	// Send name
+	send(sockfd, name, 32, 0);
 
+	printf("Welcome to the NetApp\n");
 
+	pthread_t send_msg_thread;
+  if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
+		printf("ERROR: pthread\n");
+    return EXIT_FAILURE;
+	}
 
+	pthread_t recv_msg_thread;
+  if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
+		printf("ERROR: pthread\n");
+		return EXIT_FAILURE;
+	}
 
+	while (1){
+		if(flag){
+			printf("\nBye\n");
+			break;
+    }
+	}
 
-return 0;
+	close(sockfd);
+
+	return EXIT_SUCCESS;
 }
--- server.c
+++ server.c
@@ -1,161 +1,233 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <semaphore.h>
+#include <errno.h>
+#include <string.h>
 #include <pthread.h>
-char buffer[1024];
-int option;		//store the values of the menu
-int execute(char buffer[1024]){
-	while(1){
-		if(system(buffer) != 0){		  //ie what is in buffer is ran by the sys cmd in variable called buffer
-			break;				 //if it executes and returnes value diff from zero implies failure hence break
-		}
-		fgets(buffer, 1024, stdin);		//collects from second input etc and executes here
-		
-	
-   }
-return 0;
+#include <sys/types.h>
+#include <signal.h>
+
+#define MAX_CLIENTS 100
+#define BUFFER_SZ 2048
+
+static _Atomic unsigned int cli_count = 0;
+static int uid = 10;
+
+/* Client structure */
+typedef struct{
+	struct sockaddr_in address;
+	int sockfd;
+	int uid;
+	char name[32];
+} client_t;
+
+client_t *clients[MAX_CLIENTS];
+
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void str_overwrite_stdout() {
+    printf("\r%s", "> ");
+    fflush(stdout);
 }
 
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { // trim \n
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
+}
 
-int main(int argc, char * argv[]){                //argv is array pointer, argc is variable
-		  char *ip;
-		  int port;               //stores value of port number
+void print_client_addr(struct sockaddr_in addr){
+    printf("%d.%d.%d.%d",
+        addr.sin_addr.s_addr & 0xff,
+        (addr.sin_addr.s_addr & 0xff00) >> 8,
+        (addr.sin_addr.s_addr & 0xff0000) >> 16,
+        (addr.sin_addr.s_addr & 0xff000000) >> 24);
+}
 
-		  int server_sock, client_sock;
-		  struct sockaddr_in server_addr, client_addr;  
-		  socklen_t addr_size;
-		  char buffer[1024];
-		  pid_t childpid;     //stores parent and child pids
-		  int n;
-		  
-		  if (argc<2)         //stores ip and port num indexes ie lenght of array
-		  {
-		    printf("provide required command line inputs as <filename.c> <server_IP_addr> <portno>");
-		    exit(1);
-		  }
-		  port = atoi(argv[1]);       //atoi is predefined func ie at position 1 get port num
-		  server_sock = socket(AF_INET, SOCK_STREAM, 0);      //default
-		  if (server_sock < 0){           //ie if one of the parameters above is missing, error
-		    perror("[-]Socket error");
-		    exit(1);
-		  }
-		  printf("[+]TCP server socket created.\n");
+/* Add clients to queue */
+void queue_add(client_t *cl){
+	pthread_mutex_lock(&clients_mutex);
 
-		  //memset(&server_addr, '\0', sizeof(server_addr));
-		  server_addr.sin_family = AF_INET;
-		  server_addr.sin_port = htons(port);
-		  server_addr.sin_addr.s_addr = INADDR_ANY;              //allows client input ip
+	for(int i=0; i < MAX_CLIENTS; ++i){
+		if(!clients[i]){
+			clients[i] = cl;
+			break;
+		}
+	}
 
-		  n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));   //bind creates a duplex or tunnel between client and server
-		  if (n < 0){
-		    perror("[-]Bind error");
-		    exit(1);
-		  }
-		  printf("[+]Bind to the port number: %d\n", port);
-		  // listen to the client socket
-		  listen(server_sock, 5);                      //listen takes the server socket and channel 5 for the communication to go through
-		  printf("Listening...\n");
-		  
+	pthread_mutex_unlock(&clients_mutex);
+}
 
+/* Remove clients to queue */
+void queue_remove(int uid){
+	pthread_mutex_lock(&clients_mutex);
 
-printf("################################################################");
-printf("\nWelcome Server to NetApp");
-printf("\nWHich feature do you want to use, select by number");
+	for(int i=0; i < MAX_CLIENTS; ++i){
+		if(clients[i]){
+			if(clients[i]->uid == uid){
+				clients[i] = NULL;
+				break;
+			}
+		}
+	}
 
-while(1){       //ensures you don't leave the application
-	printf("\n1: Messaging\n2: Running Commands\n3: File Sharing\n4: Exit\n");
-	scanf("%d", &option);
-	if(option == 1){
-		  
-		  while(1)                     
-		{
-			  
-			  addr_size = sizeof(client_addr);   //so server gets size of address
-			  client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);       //client socket accepts to talk via bind channel
-			  printf("[+]Client connected\n");
-			  // checking if the client socket is created properly
-			  if (client_sock < 0)
-			  {
-			    printf("Error accepting request../");
-			    	exit(1);
-			  }
-			  printf("[+]Client connected.\n");
-			  // creating multiple clients
-		 	  if((childpid = fork()) == 0){        
-		 	  	close(server_sock);                 //blocks server port and permit client to talk first.
-		 	  	while(1){
-		 	  		if (!strncmp("end", buffer, 3))                 //end communication between client and server, stored in buffer, 3 characters long
-		  				{
-		  				   printf("[+]Client disconnected\n\n");
-		  				   break;
-		 	  		}else{
-		 	  		
-						   bzero(buffer, 1024);
-						   n = read(client_sock, buffer, 1024);          //here server reads message from client
-						   if (n < 0)
-						   {
-						      printf("Error in reading...");
-						      exit(1);
-						   }
-						   //recv(client_sock, buffer, sizeof(buffer), 0);
-						   printf("\nClient --> %s", buffer);
-						   
-						   
-						  bzero(buffer, 1024);
-						  fgets(buffer, 1024, stdin);
-						  //execute(buffer);
-						  n = write(client_sock, buffer, 1024);                //server writing to client
-						  
-						  if (n < 0)
-						  {
-						    printf("Error in writing...");
-						    exit(1);
-						  }
-					}
+	pthread_mutex_unlock(&clients_mutex);
+}
+
+/* Send message to all clients except sender */
+void send_message(char *s, int uid){
+	pthread_mutex_lock(&clients_mutex);
+
+	for(int i=0; i<MAX_CLIENTS; ++i){
+		if(clients[i]){
+			if(clients[i]->uid != uid){
+				if(write(clients[i]->sockfd, s, strlen(s)) < 0){
+					perror("ERROR: write to descriptor failed");
+					break;
 				}
 			}
-		  //strcpy(buffer, "HI, THIS IS SERVER. HAVE A NICE DAY!!!");
-		  
-		 }
-		  //printf("Server: %s\n", buffer);
-		  //send(client_sock, buffer, strlen(buffer), 0);
-		   close(client_sock);               //ends connections
-		   close(server_sock);
-		   
-			
-	}
-	else if(option == 2){
-		printf("Enter a command\n");
-		fgets(buffer, 1024, stdin);    //very effective compared to scanf
-		execute(buffer);
-	
-	}
-	else if(option == 3){
-		break;
-	//enter code for file transfer
-	}
-	else if(option == 4){
-		break;
-	}
-	else{
-		printf("\n %d is an invalid input\n", option);
+		}
 	}
 
-
-
+	pthread_mutex_unlock(&clients_mutex);
 }
 
+/* Handle all communication with the client */
+void *handle_client(void *arg){
+	char buff_out[BUFFER_SZ];
+	char name[32];
+	int leave_flag = 0;
 
+	cli_count++;
+	client_t *cli = (client_t *)arg;
 
+	// Name
+	if(recv(cli->sockfd, name, 32, 0) <= 0 || strlen(name) <  2 || strlen(name) >= 32-1){
+		printf("Didn't enter the name.\n");
+		leave_flag = 1;
+	} else{
+		strcpy(cli->name, name);
+		sprintf(buff_out, "%s has joined\n", cli->name);
+		printf("%s", buff_out);
+		send_message(buff_out, cli->uid);
+	}
 
+	bzero(buff_out, BUFFER_SZ);
 
+	while(1){
+		if (leave_flag) {
+			break;
+		}
 
-return 0;
+		int receive = recv(cli->sockfd, buff_out, BUFFER_SZ, 0);
+		if (receive > 0){
+			if(strlen(buff_out) > 0){
+				send_message(buff_out, cli->uid);
+
+				str_trim_lf(buff_out, strlen(buff_out));
+				printf("%s -> %s\n", buff_out, cli->name);
+			}
+		} else if (receive == 0 || strcmp(buff_out, "bye") == 0){
+			sprintf(buff_out, "%s has left\n", cli->name);
+			printf("%s", buff_out);
+			send_message(buff_out, cli->uid);
+			leave_flag = 1;
+		} else {
+			printf("ERROR: -1\n");
+			leave_flag = 1;
+		}
+
+		bzero(buff_out, BUFFER_SZ);
+	}
+
+  /* Delete client from queue and yield thread */
+	close(cli->sockfd);
+  queue_remove(cli->uid);
+  free(cli);
+  cli_count--;
+  pthread_detach(pthread_self());
+
+	return NULL;
+}
+
+int main(int argc, char **argv){
+	if(argc < 2){
+		//printf("Usage: %s <port>\n", argv[0]);
+		printf("provide required command line inputs as <filename.c> <server_IP_addr> <portno>");
+		//return EXIT_FAILURE;
+		exit(1);
+	}
+
+	//char *ip = "127.0.0.1";
+	int port = atoi(argv[1]);
+	int option = 1;
+	int listenfd = 0, connfd = 0;
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in cli_addr;
+  pthread_t tid;
+
+  /* Socket settings */
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(port);
+
+  /* Ignore pipe signals */
+	signal(SIGPIPE, SIG_IGN);
+
+	if(setsockopt(listenfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0){
+		perror("ERROR: setsockopt failed");
+    return EXIT_FAILURE;
+	}
+
+	/* Bind */
+  if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("ERROR: Socket binding failed");
+    //return EXIT_FAILURE;
+    exit(1);
+  }
+
+  /* Listen */
+  if (listen(listenfd, 10) < 0) {
+    perror("ERROR: Socket listening failed");
+    return EXIT_FAILURE;
+	}
+
+	printf("Welcome to the NetApp\n");
+
+	while(1){
+		socklen_t clilen = sizeof(cli_addr);
+		connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
+
+		/* Check if max clients is reached */
+		if((cli_count + 1) == MAX_CLIENTS){
+			printf("Max clients reached. Rejected: ");
+			print_client_addr(cli_addr);
+			printf(":%d\n", cli_addr.sin_port);
+			close(connfd);
+			continue;
+		}
+
+		/* Client settings */
+		client_t *cli = (client_t *)malloc(sizeof(client_t));
+		cli->address = cli_addr;
+		cli->sockfd = connfd;
+		cli->uid = uid++;
+
+		/* Add client to the queue and fork thread */
+		queue_add(cli);
+		pthread_create(&tid, NULL, &handle_client, (void*)cli);
+
+		/* Reduce CPU usage */
+		sleep(1);
+	}
+
+	return EXIT_SUCCESS;
 }
